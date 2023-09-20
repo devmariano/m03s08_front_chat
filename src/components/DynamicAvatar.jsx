a74eb0c70231be939bc7f4d4cf1fb3a3d5993d61
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-09-20 11:21:12 -0300

    adcionado componente avatar dinamico e bt panic

---
+++ src/components/DynamicAvatar.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+
+export default function DynamicAvatar({ nickname }) {
+  // Gere a URL da imagem com base no nickname
+  const avatarUrl = `https://api.multiavatar.com/${nickname}.svg`;
+
+  return (
+    <img
+      src={avatarUrl}
+      alt={`Avatar de ${nickname}`}
+      style={{ width: "50px", height: "50px", borderRadius: "50%" }}
+    />
+  );
+}
+
+DynamicAvatar.propTypes = {
+  nickname: PropTypes.string.isRequired, // Espera-se que 'nickname' seja uma string obrigatória
+};
